82ee89be40f8e25a78f57a9316d4b03f3d3aa456
rabadanov.rm <rabadanov.rm@phystech.edu>
2021-02-26 00:02:28 +0000

    Solved task array

---
+++ array/array.h
@@ -0,0 +1,125 @@
+#ifndef ARRAY_ARRAY_H
+#define ARRAY_ARRAY_H
+
+#include <iterator>
+#include <stdexcept>
+
+class ArrayOutOfRange : public std::out_of_range {
+public:
+    ArrayOutOfRange() : std::out_of_range("ArrayOutOfRange") {}
+};
+
+template <class T, size_t N>
+struct Array {
+    T arr[N];
+
+    T& operator[](const size_t& index)  {
+        return arr[index];
+    }
+
+    T operator[](const size_t& index) const {
+        return arr[index];
+    }
+
+    T& Back() {
+        return arr[N-1];
+    }
+
+    T Back() const {
+        return arr[N - 1];
+    }
+
+    T& Front() {
+        return arr[0];
+    }
+
+    T Front() const {
+        return arr[0];
+    }
+
+    T* Data() {
+        return arr;
+    }
+
+    T* Data() const {
+        return arr;
+    }
+
+    size_t Size() const {
+        return N;
+    }
+
+    bool Empty() const {
+        return (N == 0);
+    }
+
+    void Fill(const T& value) {
+        for (size_t i = 0; i < N; ++i) {
+            arr[i] = value;
+        }
+    }
+
+    T& At(size_t idx) {
+        if (idx > N - 1) {
+            throw ArrayOutOfRange{};
+        } else {
+            return arr[idx];
+        }
+    }
+
+    T At(size_t idx) const {
+        if (idx > N - 1) {
+            throw ArrayOutOfRange{};
+        } else {
+            return arr[idx];
+        }
+    }
+
+    void Swap(Array<T, N>& other) {  // NOLINT
+        for (size_t i = 0; i < N; ++i) {
+            std::swap(arr[i], other.arr[i]);
+        }
+    }
+
+    bool operator<(const Array<T, N>& other) const {
+        size_t pos = 0;
+        while (pos < N && arr[pos] == other.arr[pos]) {
+            pos++;
+        }
+        return (pos < N && arr[pos] < other.arr[pos]);
+    }
+
+    bool operator==(const Array<T, N>& other) const {
+        size_t pos = 0;
+        while (pos < N && arr[pos] == other.arr[pos]) {
+            pos++;
+        }
+        return (pos == N);
+    }
+
+    bool operator!=(const Array<T, N>& other) {
+        size_t pos = 0;
+        while (pos < N && arr[pos] == other.arr[pos]) {
+            pos++;
+        }
+        return (pos != N);
+    }
+
+    bool operator>=(const Array<T, N>& other) {
+        size_t pos = 0;
+        while (pos < N && arr[pos] == other.arr[pos]) {
+            pos++;
+        }
+        return (pos < N && arr[pos] > other.arr[pos] || pos == N);
+    }
+
+    bool operator>(const Array<T, N>& other) {
+        return (*this >= other && *this != other)
+    }
+
+    bool operator<=(const Array<T, N>& other) {
+        return (*this < other || *this == other);
+    }
+};
+
+#endif  // ARRAY_ARRAY_H
