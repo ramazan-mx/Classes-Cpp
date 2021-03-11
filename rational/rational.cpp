fca89f29bb4766e20caa3687d277e72fe8386c20
rabadanov.rm <rabadanov.rm@phystech.edu>
2021-03-11 15:44:36 +0000

    Changed files rational.cpp and rational.h

--- rational/rational.cpp
+++ rational/rational.cpp
@@ -1,3 +1,167 @@
+#include <iostream>
+#include <numeric>
+#include <stdlib.h>
 #include "rational.h"
 
-//your code here
+int GCD(int u, int v) {
+    if (u < v) {
+        std::swap(u, v);
+    }
+    while (v) {
+        u %= v;
+        std::swap(u, v);
+    }
+    return u;
+}
+
+void Rational::Reduce() {
+    int gcd = GCD(num, den);
+    if (den < 0) {
+        den *= -1;
+        num *= -1;
+    }
+    num /= gcd;
+    den /= gcd;
+}
+
+Rational::Rational() : num(0), den(1) {}
+
+Rational::Rational(int other) : num(other), den(1) {}
+
+Rational::Rational(int x, int y) {
+    if (y == 0) {
+        throw RationalDivisionByZero{};
+    }
+    num = x,
+            den = y;
+    Reduce();
+}
+
+std::istream& operator>>(std::istream& is, Rational& fraction) {
+    int num, den;
+    char delimiter;
+    is >> num >> delimiter >> den;
+    fraction.SetNumerator(num);
+    fraction.SetDenominator(den);
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& out, Rational& fraction) {
+    if (fraction.GetDenominator() == 1) {
+        out << fraction.GetNumerator();
+    } else {
+        out << fraction.GetNumerator() << " / " << fraction.GetDenominator();
+    }
+    return out;
+}
+
+int Rational::GetNumerator() const {
+    return num;
+}
+
+int Rational::GetDenominator() const {
+    return den;
+}
+
+void Rational::SetNumerator(int numerator) {
+    num = numerator;
+    Reduce();
+}
+
+void Rational::SetDenominator(int denominator) {
+    den = denominator;
+    Reduce();
+}
+
+const Rational& Rational::operator+=(const Rational& other) {
+    this->SetNumerator(num * other.den + other.num * den);
+    this->SetDenominator(den * other.den);
+    Reduce();
+    return *this;
+}
+
+const Rational& Rational::operator-=(const Rational& other) {
+    this->SetNumerator(num * other.den - other.num * den);
+    this->SetDenominator(den * other.den);
+    Reduce();
+    return *this;
+};
+
+const Rational& Rational::operator/=(const Rational& other) {
+    this->SetNumerator(num * other.den);
+    this->SetDenominator(den * other.num);
+    Reduce();
+    return *this;
+};
+
+const Rational& Rational::operator*=(const Rational& other) {
+    this->SetNumerator(num * other.num);
+    this->SetDenominator(den * other.den);
+    Reduce();
+    return *this;
+}
+
+const Rational Rational::operator+(Rational& other) {
+    Rational copy = *this;
+    return copy += other;
+}
+
+const Rational Rational::operator-(Rational& other) {
+    Rational copy = *this;
+    return copy -= other;
+}
+
+const Rational Rational::operator/(Rational& other) {
+    Rational copy = *this;
+    return copy /= other;
+}
+
+const Rational Rational::operator*(Rational& other) {
+    Rational copy = *this;
+    return copy *= other;
+}
+
+const Rational Rational::operator+() {
+    return *this;
+}
+
+const Rational Rational::operator-() {
+    this->SetNumerator(-num);
+    return *this;
+}
+
+const Rational Rational::operator--() {
+    this->SetNumerator(num - den);
+    Reduce();
+    return *this;
+}
+
+const Rational Rational::operator++() {
+    this->SetNumerator(num + den);
+    Reduce();
+    return *this;
+}
+
+bool Rational::operator<(const Rational& other) const {
+    return (num * other.den < den * other.num);
+}
+
+bool Rational::operator==(const Rational& other) const {
+    return (num * other.den == den * other.num);
+}
+
+bool Rational::operator!=(const Rational& other) const {
+    return !(operator==(other));
+}
+
+bool Rational::operator>=(const Rational& other) const {
+    return !(operator<(other));
+}
+
+bool Rational::operator>(const Rational& other) const {
+    return (operator>=(other) && operator!=(other));
+}
+
+bool Rational::operator<=(const Rational& other) const {
+    return (operator<(other) || operator==(other));
+}
--- rational/rational.h
+++ rational/rational.h
@@ -1,7 +1,10 @@
+#pragma once
+
 #ifndef RATIONAL_RATIONAL_H
 #define RATIONAL_RATIONAL_H
 
 #include <util/constants.h>
+
 
 class RationalDivisionByZero : public std::runtime_error {
 public:
@@ -10,7 +13,46 @@ public:
 };
 
 class Rational {
-    //your code here
-}
+private:
+    int num;
+    int den;
+    void Reduce();
+
+public:
+    Rational();
+    Rational(int x);
+    Rational(int x, int y);
+
+    int GetNumerator() const;
+    int GetDenominator() const;
+    void SetNumerator(int x);
+    void SetDenominator(int x);
+
+    friend std::istream& operator>>(std::istream& is, Rational& fraction);
+    friend std::ostream& operator<<(std::ostream& out, Rational& fraction);
+
+    const Rational operator+(Rational& other);
+    const Rational operator-(Rational& other);
+    const Rational operator/(Rational& other);
+    const Rational operator*(Rational& other);
+
+    const Rational& operator+=(const Rational& other);
+    const Rational& operator-=(const Rational& other);
+    const Rational& operator/=(const Rational& other);
+    const Rational& operator*=(const Rational& other);
+
+    const Rational operator+();
+    const Rational operator-();
+
+    const Rational operator--();
+    const Rational operator++();
+
+    bool operator<(const Rational& x) const;
+    bool operator==(const Rational& x) const;
+    bool operator!=(const Rational& x) const;
+    bool operator>=(const Rational& x) const;
+    bool operator>(const Rational& x) const;
+    bool operator<=(const Rational& x) const;
+};
 
 #endif  // RATIONAL_RATIONAL_H
